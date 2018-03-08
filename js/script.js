ac085d82e1d05ffdfeb1c6e8838eef3ff6d7fc6f
Christian Cambizaca <christianismypapichulo@gmail.com>
2018-03-08 15:18:03 +0000

    Completed!

--- js/script.js
+++ js/script.js
@@ -13,38 +13,44 @@ $("button").click(function(){
 	 $(".print").text(result);
 });
 
-
 function sentenceToPigLatin(word) {
 	var arr = word.split(' ');
-		for (var i = 0; i < arr.length; i++){
-			if (arr[i].slice(0,1) = "b" || "c" || "d" ||  "f" || "g" || "j" || "k" || "m" || "n" || "l" || "p" || "q" || "r" ||"s" || "t" || "v" || "w" || "x" || "z" ){
-			arr[i] = arr[i] + "ay "
-			else ()
-			{
-			arr[i] = arr[i] + "cat "
-			}
+	for (var i = 0; i < arr.length; i++){
+		if(arr[i].slice(0,1) === "b" || 
+			arr[i].slice(0,1) ==="c"||
+			arr[i].slice(0,1) === "d" || 
+			arr[i].slice(0,1) === "f" || 
+			arr[i].slice(0,1) === "g" || 
+			arr[i].slice(0,1) === "h" || 
+			arr[i].slice(0,1) === "j" ||
+			arr[i].slice(0,1) === "k" || 
+			arr[i].slice(0,1) === "l" ||
+			arr[i].slice(0,1) === "m" ||
+			arr[i].slice(0,1) === "n" || 
+			arr[i].slice(0,1) === "p" || 
+			arr[i].slice(0,1) === "q" || 
+			arr[i].slice(0,1) === "r" || 
+			arr[i].slice(0,1) === "s" || 
+			arr[i].slice(0,1) === "t" || 
+			arr[i].slice(0,1) === "v" || 
+			arr[i].slice(0,1) === "w" || 
+			arr[i].slice(0,1) === "x" || 
+			arr[i].slice(0,1) === "y" || 
+			arr[i].slice(0,1) === "z"){
+			var firstletter = arr[i].slice(0,1)
+			arr[i] = arr[i].slice(1,(arr[i].length)) + firstletter + "ay"
 		}
-	return arr;
-}	
-
-
+		else {
+			arr[i] = arr[i] + "ay"	
+		}
+		//arr[i] = arr[i] + "ay ";
+		//console.log(arr[i].slice(0,1);
+	}	
+	return(arr);
+}
+})
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
-/*function ayadder(arr){
-	for (var i = 0; i < arr.length; i++){
-	return(array[i] + "ay");
-	}
-	
-}
-*/
-
-
-
-
-});
-
-
-
 
 
 
@@ -52,5 +58,3 @@ function sentenceToPigLatin(word) {
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
 	//Loops through all the words in the sentence and transforms each word
 	//It should return a transfromed sentance
-
-
